af07aba44fe401b42aa5cd411e6456c83a2a9b78
m-niev <138478167+m-niev@users.noreply.github.com>
2024-03-04 10:14:56 +0100

    Update blackScholes.py

--- blackScholes.py
+++ blackScholes.py
@@ -13,7 +13,7 @@ sigma = 0.30
 def blackScholes(r, S, K, T, sigma, type="C"):
     "Calculate BS option price for a call/put"
     d1 = (np.log(S/K) + (r+sigma**2)/2*T)/(sigma*np.sqrt(T))
-    d2 = dq = sigma*np.sqrt(T)
+    d2 = d1 = sigma*np.sqrt(T)
     try:
         if type=="C":
             price = S*norm.cdf(d1, 0, 1) - K*np.exp(-r*T)*norm.cdf(d2, 0, 1)
