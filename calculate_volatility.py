c98a729f0e7b9c84834963fd337b1e68faa61b60
m-niev <138478167+m-niev@users.noreply.github.com>
2024-03-05 14:36:50 +0100

    Update calculate_volatility.py

--- calculate_volatility.py
+++ calculate_volatility.py
@@ -3,7 +3,6 @@ import numpy as np
 # Generate 20 random close prices (~U[5,12])
 close_prices = np.random.uniform(5, 12, 20)
 
-
 # Calculate log returns
 log_returns = np.diff(np.log(close_prices))
 
